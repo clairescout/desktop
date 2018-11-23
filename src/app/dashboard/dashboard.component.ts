5ae97ac8c92f4586e0de927d5b7a364670bb2b5a
Emily Hales <emilyhales2@gmail.com>
2018-11-23 08:50:32 -0800

    asdf

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  private googleApiKey = 'AIzaSyBHGmlJTnblR5Jy58Wbhiy3p576i8BMLRw';
+  private googleBaseUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
+  private newYorkTimesApiKey = 'a846ecda81804214814cde1da594fdc6';
+  private newYorkTimesBaseUrl = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json';
+  myBooks: any;
+  loaded: boolean;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   ngOnInit() {
+    this.loaded = false;
+    this.httpClient.get(`${this.newYorkTimesBaseUrl}?api-key=${this.newYorkTimesApiKey}`).subscribe( data => {
+      const data_string = JSON.stringify(data);
+      var isbns = [];
+      var results = JSON.parse(data_string).results;
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].isbns[0] != null) {
+          isbns.push(results[i].isbns[0].isbn10)
+        }
+      }
+      // var isbn = JSON.parse(data_string).results[1].isbns[0].isbn10;
+
+      // TWO ISSUES:
+      //  for some reason, the array isn't liking the objects that come from the google api
+      //  I need to do something asynchronous because myBooks is being set before books is done computing
+      var books = []
+      for (var i = 0; i < isbns.length; i++) {
+        var isbn = isbns[i];
+        this.httpClient.get(`${this.googleBaseUrl}?isbn:${isbn}&key=${this.googleApiKey}`).subscribe( data => {
+          const data_string = JSON.stringify(data);
+          books.push(JSON.parse(data_string).items[0].volumeInfo);
+          // books.push(1)
+          // console.log(books.length)
+        });
+      }
+      // this.myBooks = books;
+      // console.log(books.length)
+      this.loaded = true;
+
+    })
   }
 
 }
