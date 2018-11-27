a9bd2958a125a72345a875ec6d24dbbf9eb4f80d
Emily Hales <emilyhales2@gmail.com>
2018-11-27 15:33:32 -0700

    not paying attention to class at all lol

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { MatCardModule, MatProgressBarModule, MatDialogModule, MatButtonModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list'
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatButtonModule
   ]
 })
 export class DashboardModule { }
