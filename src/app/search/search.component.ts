ea53386fa5e879f663db55c2acd0c262354fb1cd
Emily Hales <emilyhales2@gmail.com>
2018-11-28 08:47:00 -0700

    added another api key

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,9 +12,10 @@ import { Router } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
   private searchField: any;
+  private googleApiKey = 'AIzaSyBXzONnrOqcH0xQg_6ZfZnJSW69Ipbexu8';
   // private googleApiKey = 'AIzaSyApxADsUGvvmAhQw4WhZjPrEduztHjhtms';
   // private googleApiKey = 'AIzaSyBHGmlJTnblR5Jy58Wbhiy3p576i8BMLRw';
-  private googleApiKey = 'AIzaSyCaj2CVGkrMwM9__MjQLBaEbCoGKFWVlAw';
+  // private googleApiKey = 'AIzaSyCaj2CVGkrMwM9__MjQLBaEbCoGKFWVlAw';
   private googleBaseUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
   searchResults: any;
   loaded: boolean;
