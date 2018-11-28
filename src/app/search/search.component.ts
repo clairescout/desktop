5c68dbebd214b7ecae9ce04950e9ef878ecc9a0c
Emily Hales <emilyhales2@gmail.com>
2018-11-27 18:56:32 -0700

    temp

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../services/storage-service.service';
 
@@ -8,12 +9,23 @@ import { StorageService } from '../services/storage-service.service';
 })
 export class SearchComponent implements OnInit {
   private searchField: any;
+  // private googleApiKey = 'AIzaSyApxADsUGvvmAhQw4WhZjPrEduztHjhtms';
+  private googleApiKey = 'AIzaSyBHGmlJTnblR5Jy58Wbhiy3p576i8BMLRw';
+  private googleBaseUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
+  searchResults: any;
+  loaded: boolean;
 
-  constructor(private storageService: StorageService) { }
+  constructor(private httpClient: HttpClient,
+              private storageService: StorageService) { }
 
   ngOnInit() {
     this.searchField = this.storageService.getSearchField();
-    console.log(this.searchField);
+    this.httpClient.get(`${this.googleBaseUrl}${this.searchField}&key=${this.googleApiKey}`).subscribe( data => {
+      const data_string = JSON.stringify(data);
+      // this.currentBook = JSON.parse(data_string).items[0].volumeInfo;
+      console.log(JSON.parse(data_string).items.length);
+      this.loaded = true;
+    });
   }
 
 }
