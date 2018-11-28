4730018c86cb2b02c948805fd8ad9f82728d1e06
Emily Hales <emilyhales2@gmail.com>
2018-11-27 21:49:07 -0700

    did stuff with the search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material';
 import { StorageService } from '../services/storage-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -10,22 +13,29 @@ import { StorageService } from '../services/storage-service.service';
 export class SearchComponent implements OnInit {
   private searchField: any;
   // private googleApiKey = 'AIzaSyApxADsUGvvmAhQw4WhZjPrEduztHjhtms';
-  private googleApiKey = 'AIzaSyBHGmlJTnblR5Jy58Wbhiy3p576i8BMLRw';
+  // private googleApiKey = 'AIzaSyBHGmlJTnblR5Jy58Wbhiy3p576i8BMLRw';
+  private googleApiKey = 'AIzaSyCaj2CVGkrMwM9__MjQLBaEbCoGKFWVlAw';
   private googleBaseUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
   searchResults: any;
   loaded: boolean;
 
   constructor(private httpClient: HttpClient,
-              private storageService: StorageService) { }
+              private storageService: StorageService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.searchResults = [];
     this.searchField = this.storageService.getSearchField();
     this.httpClient.get(`${this.googleBaseUrl}${this.searchField}&key=${this.googleApiKey}`).subscribe( data => {
       const data_string = JSON.stringify(data);
-      // this.currentBook = JSON.parse(data_string).items[0].volumeInfo;
-      console.log(JSON.parse(data_string).items.length);
+      this.searchResults = JSON.parse(data_string).items;
       this.loaded = true;
     });
+  }
+
+  goToBookDetails(book) {
+    this.storageService.setIsbn(book.industryIdentifiers[0].identifier); // TODO: is book.isbn right?
+    this.router.navigateByUrl('/book-details');
   }
 
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
+import { MatCardModule, MatProgressSpinnerModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { StorageService } from '../services/storage-service.service';
 
 @NgModule({
   declarations: [SearchComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   providers: [StorageService]
 })
