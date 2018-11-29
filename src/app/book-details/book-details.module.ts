8d7b0f58095becbe43f34c13fcb4892112eddc1a
Claire Gammon <claire.gammon@gmail.com>
2018-11-28 17:31:41 -0700

    css changes again

--- src/app/book-details/book-details.module.ts
+++ src/app/book-details/book-details.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookDetailsComponent } from './book-details.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatCardModule, MatProgressBarModule, MatDialogModule, MatButtonModule, MatProgressSpinnerModule, 
+import { MatCardModule, MatProgressBarModule, MatDialogModule, MatButtonModule, MatProgressSpinnerModule,
   MatExpansionModule } from '@angular/material';
 import { ContentModalComponent } from './content-modal/content-modal.component';
 import { StorageService } from '../services/storage-service.service';
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,14 +13,15 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
   // private googleApiKey = 'AIzaSyBXzONnrOqcH0xQg_6ZfZnJSW69Ipbexu8';
-  private googleApiKey = 'AIzaSyApxADsUGvvmAhQw4WhZjPrEduztHjhtms';
-  // private googleApiKey = 'AIzaSyBHGmlJTnblR5Jy58Wbhiy3p576i8BMLRw';
+  // private googleApiKey = 'AIzaSyApxADsUGvvmAhQw4WhZjPrEduztHjhtms';
+  private googleApiKey = 'AIzaSyBHGmlJTnblR5Jy58Wbhiy3p576i8BMLRw';
   // private googleApiKey = 'AIzaSyCaj2CVGkrMwM9__MjQLBaEbCoGKFWVlAw';
   private googleBaseUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
   private newYorkTimesApiKey = 'a846ecda81804214814cde1da594fdc6';
   private newYorkTimesBaseUrl = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json';
   myBooks: any;
   loaded: boolean;
+  reviews: any;
 
   constructor(private httpClient: HttpClient,
               private storageService: StorageService,
@@ -28,6 +29,7 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initReviews();
     this.loaded = false;
     this.myBooks = [];
     this.httpClient.get(`${this.newYorkTimesBaseUrl}?api-key=${this.newYorkTimesApiKey}`).subscribe( data => {
@@ -83,6 +85,22 @@ export class DashboardComponent implements OnInit {
   goToBookDetails(book) {
     this.storageService.setIsbn(book.industryIdentifiers[0].identifier); // TODO: is book.isbn right?
     this.router.navigateByUrl('/book-details');
+  }
+
+  initReviews() {
+    this.reviews = [
+      {title: 'Violence', scores: [4, 4, 4, 4, 5], reviews: ['lots of violence', 'shooting and guns and punching']},
+      {title: 'Sex', scores: [1, 1, 2, 4, 1], reviews: ['A man and a woman talk about having sex', 'A naked man']},
+      {title: 'Language', scores: [3, 3, 3, 3, 3], reviews: ['a few cuss words', 'typical usual cussing']},
+      {title: 'Drugs/Alcohol', scores: [2, 2, 2, 2, 2], reviews: ['one character has a glass of wine']},
+    ];
+  }
+
+  getRating(review) {
+    const scores = review.scores;
+    const total = scores.reduce((a, b) => a + b);
+    const average = (total / scores.length);
+    return (average / 5) * 100;
   }
 
 }
